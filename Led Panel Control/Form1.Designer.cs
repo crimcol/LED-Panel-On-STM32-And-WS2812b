383db2e7b6209bfa6f2aa94ab615ba6a26a1e7f6
volodymyr.liashenko <crimcol2@gmail.com>
2016-11-11 09:40:04 +0200

    - start to implement application to control led panel;

---
+++ Led Panel Control/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Led_Panel_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panelSelectedColor = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1MinSize = 200;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panelSelectedColor);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(917, 337);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select COM port:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(106, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(88, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "COM9";
+            // 
+            // panelSelectedColor
+            // 
+            this.panelSelectedColor.BackColor = System.Drawing.SystemColors.Window;
+            this.panelSelectedColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSelectedColor.Location = new System.Drawing.Point(593, 13);
+            this.panelSelectedColor.Name = "panelSelectedColor";
+            this.panelSelectedColor.Size = new System.Drawing.Size(108, 23);
+            this.panelSelectedColor.TabIndex = 4;
+            this.panelSelectedColor.DoubleClick += new System.EventHandler(this.panelSelectedColor_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(509, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Selected color:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(509, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(494, 255);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 337);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panelSelectedColor;
+    }
+}
+
---
+++ Led Panel Control/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Led_Panel_Control
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _activeSerialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AppInit();
+        }
+
+        private void AppInit()
+        {
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private SerialPort CreateSerialPort(string portName)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = portName;
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            return serialPort;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = (SerialPort)sender;
+            string received = port.ReadExisting();
+
+            SetReceivedText(received);
+        }
+
+        private void SendText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return;
+            }
+
+            _activeSerialPort.Write(text);
+            SetSendText(text);
+        }
+
+        private void SendColor(Color color)
+        {
+            string colorRGBText = string.Format("{0}{1}{2}", (char)color.R, (char)color.G, (char)color.B);
+            SendText(colorRGBText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_activeSerialPort == null)
+            {
+                _activeSerialPort.Close();
+                _activeSerialPort.Dispose();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _activeSerialPort = CreateSerialPort(comboBox1.Text);
+            _activeSerialPort.Open();
+            SetInfoText("Port opened.");
+        }
+
+        private void SetReceivedText(string text)
+        {
+            Action action = () => 
+            {
+                text = text.Replace("\r", "<CR>").Replace("\n", "<LF>");
+                string formattedText = string.Format("[{0:H:mm:ss.ffff}] <<< {1}\n", DateTime.Now, text);
+                richTextBox1.AppendText(formattedText);
+            };
+            richTextBox1.Invoke(action);
+        }
+
+        private void SetSendText(string text, Color? color = null)
+        {
+            string formattedText = string.Format("[{0:H:mm:ss.ffff}] >>> {1}", DateTime.Now, text);
+            SetText(formattedText, color);
+        }
+        private void SetText(string text, Color? color = null)
+        {
+            string formattedText = string.Format("{0}\n", text);
+            Color currentFontColor = richTextBox1.ForeColor;
+
+            if (color.HasValue)
+            {
+                richTextBox1.ForeColor = color.Value;
+            }
+
+            richTextBox1.AppendText(formattedText);
+
+            if (color.HasValue)
+            {
+                richTextBox1.ForeColor = currentFontColor;
+            }
+        }
+
+        private void SetInfoText(string text)
+        {
+            string formattedText = string.Format("[{0:H:mm:ss.ffff}] {1}\n", DateTime.Now, text);
+            SetText(formattedText, Color.DarkRed);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            SendText(text);
+        }
+
+        private void panelSelectedColor_DoubleClick(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                panelSelectedColor.BackColor = colorDialog1.Color;
+                SendColor(colorDialog1.Color);
+            }
+        }
+    }
+}
